25f403164860950fa0403e302fc39c1f232efc11
Alan Storm <astorm@alanstorm.com>
2015-11-19 15:15:59 -0800

    Issue #9 -- grab entire ometria system configuration ui tree

--- app/code/Ometria/Api/Controller/V1/Get/Settings.php
+++ app/code/Ometria/Api/Controller/V1/Get/Settings.php
@@ -3,18 +3,24 @@ namespace Ometria\Api\Controller\V1\Get;
 class Settings extends \Magento\Framework\App\Action\Action
 {
     protected $resultJsonFactory;
+    protected $scopeConfig;
     
+    const CONFIG_TOP = 'ometria';
 	public function __construct(
 		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+		\Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
 	) {
 		parent::__construct($context);
 		$this->resultJsonFactory = $resultJsonFactory;
+		$this->scopeConfig = $scopeConfig;
 	}
 	
     public function execute()
     {
-		$result = $this->resultJsonFactory->create();
-		return $result->setData(['success' => true]);
+        $values = $this->scopeConfig->getValue(self::CONFIG_TOP);    
+        $data   = [self::CONFIG_TOP=>$values];
+		$result = $this->resultJsonFactory->create();		
+		return $result->setData($data);
     }    
 }
