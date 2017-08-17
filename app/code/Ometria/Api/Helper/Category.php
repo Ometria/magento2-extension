8ca50886785693fb3de7195145d7505bfd72fe2f
Al James <al@ometria.com>
2017-08-17 12:11:44 +0100

    Attempted fix for case where a category has been deleted via SQL and data is inconsitent

--- app/code/Ometria/Api/Helper/Category.php
+++ app/code/Ometria/Api/Helper/Category.php
@@ -8,20 +8,27 @@ class Category
     {
         $this->categoryRepository = $categoryRepository;
     }
-    
+
     public function getOmetriaAttributeFromCategoryIds($ids)
     {
-        return array_map(function($id){
-            $category = $this->categoryRepository->get($id);
-            
-            return [
-                'type'      =>'category',
-                'id'        =>$id,
-                'url_key'   =>$category->getUrlKey(),
-                'url_path'  =>$category->getUrlPath(),
-                'label'     =>$category->getName()
-            ];        
-        }, $ids);
-        
+        $ret = [];
+
+        foreach($ids as $id){
+            try{
+                $category = $this->categoryRepository->get($id);
+
+                if ($category) $ret[] = [
+                    'type'      =>'category',
+                    'id'        =>$id,
+                    'url_key'   =>$category->getUrlKey(),
+                    'url_path'  =>$category->getUrlPath(),
+                    'label'     =>$category->getName()
+                ];
+            } catch(\Exception $e) {
+                // pass, prevent issues with missing categories
+            }
+        }
+
+        return $ret;
     }
 }
