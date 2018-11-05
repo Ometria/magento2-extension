47e8227767c5825d5ec18223d24bc7e24aa828b2
Joseph McDermott <code@josephmcdermott.co.uk>
2018-11-05 13:29:00 +0000

    Added new totals for subtotal, discount and tax (WIP as might need to be in V2 for backward-compatibility)

--- app/code/Ometria/Api/Controller/V1/Orders.php
+++ app/code/Ometria/Api/Controller/V1/Orders.php
@@ -229,7 +229,17 @@ class Orders extends Base
                 $new["sku"]               = $line_item['parent']['sku'];
                 $new["quantity"]          = $line_item['parent']['qty_ordered'];
                 $new["unit_price"]        = $line_item['parent']['base_price'];
-                $new["total"]             = $line_item['parent']['row_total'];
+                $new["subtotal"]          = $line_item['parent']['base_row_total'];
+                $new["discount"]          = $line_item['parent']['base_discount_amount'];
+                $new["discount_percent"]  = $line_item['parent']['discount_percent'];
+                $new["tax"]               = $line_item['parent']['base_tax_amount'];
+                $new["tax_percent"]       = $line_item['parent']['tax_percent'];
+                $new["total"]             = (string)(
+                      $line_item['parent']['base_row_total']
+                    + $line_item['parent']['base_tax_amount']
+                    - $line_item['parent']['base_discount_amount']
+                );
+                
                 $new_line_items[] = $new;
             }
             $item['lineitems'] = $new_line_items;  
