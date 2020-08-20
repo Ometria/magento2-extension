f25ea1607211e2b50a5c07cfdf4e42824d9a06d6
Chris Pook <chris.pook@absolutecommerce.co.uk>
2020-08-20 21:00:00 +0000

    M2-1 Added "count" parameter to retrieve count of entities from Attribute Types API.

--- app/code/Ometria/Api/Helper/Format/V1/Attribute/Types.php
+++ app/code/Ometria/Api/Helper/Format/V1/Attribute/Types.php
@@ -5,10 +5,10 @@ class Types
     static public function getBlankArray()
     {
         return [
-            "id" =>null,
-            "attribute_type"=>null,
-            "title"=>null,
-            "attribute_code"=>null
+            "id" => null,
+            "attribute_type" => null,
+            "title" => null,
+            "attribute_code" => null
         ];
     }
 }
